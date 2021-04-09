e26a78c8c80f082ffdea97b15831294dc37d2d5c
Kishlay Kumar <kishlay.kumar@publicissapient.com>
2021-04-10 00:44:01 +0530

    Basic Details

--- src/components/Application/BasicDetails/index.js
+++ src/components/Application/BasicDetails/index.js
@@ -3,17 +3,36 @@ import React from "react";
 const index = (props) => {
   return (
     <div className="basic">
-      
       <div className="profile">
         <div className="profilepic">
-            
+          <img
+            src="https://illumesense.com/resources/illumesense/style/img/website/profile-picture-blanks/male-profile.jpg"
+            alt="profile"
+            style={{
+              height: "200px",
+              width: "200px",
+              margin: "10px",
+              boxShadow:
+                "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+            }}
+          />
         </div>
-        <div className="profiledetails">
-          <p>Name: Lorem Ipsum</p>
-          <p>Email: Lorem@Ipsum.com</p>
-          <p>Father's Name: Lorem Ipsum</p>
-          <p>Marital Status: Lorem Ipsum</p>
-          <p>Address: Lorem Ipsum</p>
+        <div className="user-detail" style={{ marginLeft: "30px" }}>
+          <p>
+            <span className="bold">Name: </span> Lorem Ipsum
+          </p>
+          <p>
+            <span className="bold">Email: </span> Lorem@Ipsum.com
+          </p>
+          <p>
+            <span className="bold">Father's Name: </span> Lorem Ipsum
+          </p>
+          <p>
+            <span className="bold">Martial Status: </span> Lorem Ipsum
+          </p>
+          <p>
+            <span className="bold">Address: </span> Lorem Ipsum
+          </p>
         </div>
       </div>
       <div></div>
---
+++ src/components/Application/CreditScore/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CreditScore = (props) => {
+  return (
+    <div>
+      <p>
+        <span className="bold">No. of Dependent: </span> Lorem Ipsum
+      </p>
+      <p>
+        <span className="bold">Phone: </span> 1234567890
+      </p>
+      <p>
+        <span className="bold">PanCard: </span> Lorem Ipsum
+      </p>
+      <p>
+        <span className="bold">Residence Status: </span> Lorem Ipsum
+      </p>
+    </div>
+  );
+};
+
+export default CreditScore;
